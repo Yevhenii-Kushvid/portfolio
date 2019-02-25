0d230ae5511e933eca4c4d26bcd7952a28e995b9
Yevhenii Kushvid <pathfinder404@gmail.com>
2019-02-25 12:04:53 +0200

    created basic object Project

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_24_120559) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "duration"
+    t.integer "team_size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
